b6d0f4cb98cd02bda29bebf79a3da988c3949cab
Drajor <drajoreq@gmail.com>
2016-06-05 22:19:24 +1000

    Old changes

--- SpellsConvert/main.cpp
+++ SpellsConvert/main.cpp
@@ -2,11 +2,163 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <list>
 #include <sstream>
+#include <array>
 #include "../Dependencies/tinyxml/tinyxml.h"
 
 typedef std::string String;
 typedef std::stringstream StringStream;
+
+static const auto NumClasses = 16;
+static const auto NumEffects = 12;
+static const auto NumComponents = 4;
+static const auto NumFields = 215;
+typedef std::array<String, NumFields> Spell;
+typedef std::vector<Spell*> SpellFile;
+
+namespace FieldIndex {
+	enum FieldIndex : int {
+		ID,
+		Name,
+		Player,
+		TeleportZone,
+		YouCast,
+		OtherCasts,
+		CastOnYou,
+		CastOnOther,
+		FadeMessage,
+		Range,
+		AOERange,
+		PushBack,
+		PushUp,
+		CastTime,
+		RecoveryTime,
+		RecastTime,
+		BuffDurationFormula,
+		BuffDuration,
+		AOEDuration,
+		ManaCost,
+		EffectBase1,
+		EffectBase2,
+		EffectBase3,
+		EffectBase4,
+		EffectBase5,
+		EffectBase6,
+		EffectBase7,
+		EffectBase8,
+		EffectBase9,
+		EffectBase10,
+		EffectBase11,
+		EffectBase12,
+		EffectLimit1,
+		EffectLimit2,
+		EffectLimit3,
+		EffectLimit4,
+		EffectLimit5,
+		EffectLimit6,
+		EffectLimit7,
+		EffectLimit8,
+		EffectLimit9,
+		EffectLimit10,
+		EffectLimit11,
+		EffectLimit12,
+		EffectMax1,
+		EffectMax2,
+		EffectMax3,
+		EffectMax4,
+		EffectMax5,
+		EffectMax6,
+		EffectMax7,
+		EffectMax8,
+		EffectMax9,
+		EffectMax10,
+		EffectMax11,
+		EffectMax12,
+		Icon,
+		MemIcon,
+		ComponentID_1,
+		ComponentID_2,
+		ComponentID_3,
+		ComponentID_4,
+		ComponentCount_1,
+		ComponentCount_2,
+		ComponentCount_3,
+		ComponentCount_4,
+		NoExpendReagent_1,
+		NoExpendReagent_2,
+		NoExpendReagent_3,
+		NoExpendReagent_4,
+		EffectFormula1,
+		EffectFormula2,
+		EffectFormula3,
+		EffectFormula4,
+		EffectFormula5,
+		EffectFormula6,
+		EffectFormula7,
+		EffectFormula8,
+		EffectFormula9,
+		EffectFormula10,
+		EffectFormula11,
+		EffectFormula12,
+		LightType,
+		GoodEffect,
+		Activated,
+		ResistType,
+		EffectID1,
+		EffectID2,
+		EffectID3,
+		EffectID4,
+		EffectID5,
+		EffectID6,
+		EffectID7,
+		EffectID8,
+		EffectID9,
+		EffectID10,
+		EffectID11,
+		EffectID12,
+		TargetType,
+		BaseDifficulty,
+		ZoneType,
+		EnvironmentType,
+		TimeOfDay,
+		UNK,
+		WAR_Level,
+		CLR_Level,
+		PAL_Level,
+		RNG_Level,
+		SHD_Level,
+		DRU_Level,
+		MNK_Level,
+		BRD_Level,
+		ROG_Level,
+		SHM_Level,
+		NEC_Level,
+		WIZ_Level,
+		MAG_Level,
+		ENC_Level,
+		BST_Level,
+		BER_Level,
+	};
+}
+std::vector<String> Classes = {
+	"war",
+	"clr",
+	"pal",
+	"rng",
+	"shd",
+	"dru",
+	"mnk",
+	"brd",
+	"rog",
+	"shm",
+	"nec",
+	"wiz",
+	"mag",
+	"enc",
+	"bst",
+	"ber",
+};
 
 std::vector<String> FieldNames = {
 	"id",
@@ -258,6 +410,14 @@ std::vector<String> &split(const String &s, char delim, std::vector<String> &ele
 	return elems;
 }
 
+void spellLineSplit(const String& pString, Spell& pSpell) {
+	StringStream ss(pString);
+	for (auto i = 0; i < NumFields; i++) {
+		std::getline(ss, pSpell[i], '^');
+	}
+}
+
+
 // Thank you: http://stackoverflow.com/questions/236129/how-to-split-a-string-in-c
 std::vector<String> split(const String &s, char delim) {
 	std::vector<String> elems;
@@ -317,22 +477,146 @@ namespace Convert {
 	}
 	namespace Text {
 		const bool _toXMLImpl(std::ifstream& pInput, TiXmlDocument* pOutput) {
-			if (!pInput) { return false; }
+			if (!pInput) return false;
 
 			auto spellsElement = static_cast<TiXmlElement*>(pOutput->LinkEndChild(new TiXmlElement("spells")));
 
-			// Iterate lines
 			String line;
+			auto c = 0;
 			while (std::getline(pInput, line)) {
+				if (c >= 100) break;
+				c++;
+
+				Spell s;
+				spellLineSplit(line, s);
+
 				auto spellElement = static_cast<TiXmlElement*>(spellsElement->LinkEndChild(new TiXmlElement("spell")));
-				auto field = 0;
-				std::vector<String> values = split(line, '^');
-				for (auto i : values) {
-					spellElement->SetAttribute(FieldNames[field].c_str(), i.c_str());
-					field++;
+				spellElement->SetAttribute("id", s[FieldIndex::ID].c_str());
+				spellElement->SetAttribute("name", s[FieldIndex::Name].c_str());
+				spellElement->SetAttribute("mana", s[FieldIndex::ManaCost].c_str());
+				spellElement->SetAttribute("target", s[FieldIndex::TargetType].c_str());
+				spellElement->SetAttribute("range", s[FieldIndex::Range].c_str());
+				
+				if (s[FieldIndex::ResistType] != "0")
+					spellElement->SetAttribute("resist", s[FieldIndex::ResistType].c_str());
+
+				//spellElement->SetAttribute("zone", s[FieldIndex::ZoneType].c_str());
+				//spellElement->SetAttribute("env", s[FieldIndex::EnvironmentType].c_str());
+
+				// Classes.
+				auto classesElement = static_cast<TiXmlElement*>(spellElement->LinkEndChild(new TiXmlElement("classes")));
+				for (auto i = 0; i < NumClasses; i++) {
+					if (s[FieldIndex::WAR_Level + i] == "255") continue;
+					classesElement->SetAttribute(Classes[i].c_str(), s[FieldIndex::WAR_Level + i].c_str());
 				}
-				line.clear();
+
+				// Effects.
+				auto effectsElement = static_cast<TiXmlElement*>(spellElement->LinkEndChild(new TiXmlElement("effects")));
+				for (auto i = 0; i < NumEffects; i++) {
+					if (s[FieldIndex::EffectID1 + i] == "254") continue;
+
+					auto effectElement = static_cast<TiXmlElement*>(effectsElement->LinkEndChild(new TiXmlElement("effect")));
+
+					// ID
+					effectElement->SetAttribute("type", s[FieldIndex::EffectID1 + i].c_str());
+					// Formula
+					effectElement->SetAttribute("formula", s[FieldIndex::EffectFormula1 + i].c_str());
+					// Base
+					effectElement->SetAttribute("base", s[FieldIndex::EffectBase1 + i].c_str());
+					// Limit
+					effectElement->SetAttribute("limit", s[FieldIndex::EffectLimit1 + i].c_str());
+					// Max
+					effectElement->SetAttribute("max", s[FieldIndex::EffectMax1 + i].c_str());
+				}
+				// Strip element if the spell has no effects.
+				if (effectsElement->NoChildren())
+					spellElement->RemoveChild(effectsElement);
+
 			}
+
+			return true;
+			//if (!pInput) { return false; }
+
+			//auto spellsElement = static_cast<TiXmlElement*>(pOutput->LinkEndChild(new TiXmlElement("spells")));
+
+			//static const std::list<std::pair<int, int>> ignore = {
+			//	{ FieldIndex::EffectBase1, FieldIndex::EffectMax12 },
+			//	{ FieldIndex::ComponentID_1, FieldIndex::ComponentCount_4 },
+			//	{ FieldIndex::EffectFormula1, FieldIndex::EffectFormula12 },
+			//	{ FieldIndex::EffectID1, FieldIndex::EffectID12 },
+			//};
+			//auto inIgnored = [=](const int pFieldID){
+			//	for (auto i : ignore) {
+			//		if (pFieldID >= i.first && pFieldID <= i.second)
+			//			return true;
+			//	}
+			//	return false;
+			//};
+
+			//// Iterate lines
+			//String line;
+			//auto count = 1;
+			//while (std::getline(pInput, line)) {
+			//	if (count >= 1000) break;
+			//	std::cout << count << std::endl;
+			//	auto spellElement = static_cast<TiXmlElement*>(spellsElement->LinkEndChild(new TiXmlElement("spell")));
+			//	auto field = 0;
+			//	std::vector<String> values = split(line, '^');
+			//	for (auto i : values) {
+			//		if (inIgnored(field)) { field++; continue; }
+			//		if (i.empty()) { field++; continue; }
+
+			//		spellElement->SetAttribute(FieldNames[field].c_str(), i.c_str());
+			//		field++;
+			//	}
+
+			//	// Write effects.
+			//	auto effectsElement = new TiXmlElement("effects");
+			//	auto effectCount = 0;
+			//	for (auto i = 0; i < 12; i++) {
+			//		// Check for non 254 effect ID.
+			//		if (values[FieldIndex::EffectID1 + i] == "254") continue;
+			//		effectCount++;
+
+			//		auto effectElement = static_cast<TiXmlElement*>(effectsElement->LinkEndChild(new TiXmlElement("effect")));
+			//		// ID
+			//		effectElement->SetAttribute("id", values[FieldIndex::EffectID1 + i].c_str());
+			//		// Formula
+			//		effectElement->SetAttribute("formula", values[FieldIndex::EffectFormula1 + i].c_str());
+			//		// Base
+			//		effectElement->SetAttribute("base", values[FieldIndex::EffectBase1 + i].c_str());
+			//		// Limit
+			//		effectElement->SetAttribute("limit", values[FieldIndex::EffectLimit1 + i].c_str());
+			//		// Max
+			//		effectElement->SetAttribute("max", values[FieldIndex::EffectMax1 + i].c_str());
+			//	}
+			//	if (effectCount)
+			//		spellElement->LinkEndChild(effectsElement);
+			//	else
+			//		delete effectsElement;
+
+			//	// Write Components.
+			//	auto componentsElement = new TiXmlElement("components");
+			//	auto componentCount = 0;
+			//	for (auto i = 0; i < 4; i++) {
+			//		// Check for non -1 component item ID.
+			//		if (values[FieldIndex::ComponentID_1 + i] == "-1") continue;
+			//		componentCount++;
+
+			//		auto componentElement = static_cast<TiXmlElement*>(componentsElement->LinkEndChild(new TiXmlElement("component")));
+			//		// Item ID.
+			//		componentElement->SetAttribute("id", values[FieldIndex::ComponentID_1 + i].c_str());
+			//		// Item count.
+			//		componentElement->SetAttribute("count", values[FieldIndex::ComponentCount_1 + i].c_str());
+			//	}
+			//	if (componentCount)
+			//		spellElement->LinkEndChild(componentsElement);
+			//	else
+			//		delete componentsElement;
+
+			//	line.clear();
+			//	count++;
+			//}
 
 			return true;
 		}
@@ -350,11 +634,13 @@ namespace Convert {
 			const bool success = _toXMLImpl(input, output);
 			bool saved = false;
 
+			std::cout << "Saving...";
 			if (success)
 				saved = output->SaveFile();
 
 			input.close();
 			delete output;
+			std::cout << "Done" << std::endl;
 
 			return success && saved;
 		}
@@ -362,6 +648,8 @@ namespace Convert {
 }
 
 void main() {
-	Convert::XML::toText("spells.xml", "spells_us.txt");
+	//Convert::XML::toText("spells.xml", "spells_us.txt");
 	Convert::Text::toXML("spells_us.txt", "spells.xml");
+
+	//std::cout << FieldNames[104] << std::endl;
 }
